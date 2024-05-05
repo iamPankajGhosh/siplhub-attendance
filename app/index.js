7498cf2aa03fb75d042a769dc50d60260c5d9a1b
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-05-05 23:56:17 +0530

    initial setup done

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const welcomeScreen = () => {
+  return (
+    <View>
+      <Text>welcomeScreen</Text>
+    </View>
+  );
+};
+
+export default welcomeScreen;
